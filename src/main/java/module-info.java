9129a6915691449c45a5f703c9ec679fd985b478
Sia Yu <ongsiayu18@gmail.com>
2025-06-16 18:23:34 +0800

    Fixed the module-info.java issues. App could launch now.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,13 @@
 module org.tcms {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires com.dlsc.formsfx;
     requires com.opencsv;
 
     opens org.tcms to javafx.fxml;
     exports org.tcms;
+    exports org.tcms.controller;
+    opens org.tcms.controller to javafx.fxml;
+    exports org.tcms.component;
+    opens org.tcms.component to javafx.fxml;
 }
