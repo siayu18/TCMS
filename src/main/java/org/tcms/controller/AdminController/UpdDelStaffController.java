faeaab50fb6659a4ff269feb955144c6444f90d8
haojerhh <haojerh@gmail.com>
2025-07-11 11:21:17 +0800

    refactored subject.csv

--- src/main/java/org/tcms/controller/AdminController/UpdDelStaffController.java
+++ src/main/java/org/tcms/controller/AdminController/UpdDelStaffController.java
@@ -83,7 +83,7 @@ public class UpdDelStaffController {
                     }
 
 
-                    // 3. Replace tutor ID with "NO TUTOR" in tutor.csv
+                    // 3. Replace tutor ID with "NO TUTOR" in subject.csv
                     tutorService.markTutorAsDeletedInTutorCSV(selectedAccountID);
 
                     // 4. Replace tutor ID with "NO TUTOR" in tuitionclass.csv
